9c948edce7e202575e79548eb418d65b6da949f5
jbeedle19 <josh.beedle@gmail.com>
2020-11-07 12:49:23 -0500

    created array to hold questions/answers

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,31 @@
 // Variables Here:
 
+// Array for questions
+// Change value of a: to be 1, 2, 3, or 4
+var questions = [
+    {
+        q: 'How do you save data to localStorage?',
+        a: 'setItem()'
+    },
+    {
+        q: '________ converts a JavaScript object or value to a JSON string.',
+        a: 'JSON.stringify'
+    },
+    {
+        q: 'JSON stands for:',
+        a: 'JavaScript Object Notation'
+    },
+    {
+        q: 'JavaScript arrays star at index ___.',
+        a: '0'
+    },
+    {
+        q:'Which is not a correct DOM event to use with addEventListener?',
+        a: 'addEventListner("delete", function());'
+    }
+]
+
+// Quiz Questions - put in an array
 // Functions Here:
 
 // Event Listeners Here:
