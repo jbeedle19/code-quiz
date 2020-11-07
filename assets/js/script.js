d282130477a99d06cd0049325dbe8a629bb4db1d
jbeedle19 <josh.beedle@gmail.com>
2020-11-07 13:28:24 -0500

    added view high score/time to header element

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,25 +5,28 @@
 var questions = [
     {
         q: 'How do you save data to localStorage?',
-        a: 'setItem()'
+        a: '3 - setItem()'
     },
     {
         q: '________ converts a JavaScript object or value to a JSON string.',
-        a: 'JSON.stringify'
+        a: '1 - JSON.stringify'
     },
     {
         q: 'JSON stands for:',
-        a: 'JavaScript Object Notation'
+        a: '4 - JavaScript Object Notation'
     },
     {
         q: 'JavaScript arrays star at index ___.',
-        a: '0'
+        a: '2 - 0'
     },
     {
         q:'Which is not a correct DOM event to use with addEventListener?',
-        a: 'addEventListner("delete", function());'
+        a: '3 - addEventListner("delete", function());'
     }
 ]
+
+// Variable to hold Time and start at 0
+var time = 0;
 
 // Quiz Questions - put in an array
 // Functions Here:
